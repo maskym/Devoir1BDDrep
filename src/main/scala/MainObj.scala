47968118c19ce0d79fe1e3cdfeea3dd833814c81
Maxime Defrances <34211283+ubowt571@users.noreply.github.com>
2019-10-09 10:09:16 -0400

    change global structure

--- src/main/scala/MainObj.scala
+++ src/main/scala/MainObj.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection.mutable.ListBuffer
@@ -14,94 +15,59 @@ object MainObj extends  App {
 
   var time_debut = System.currentTimeMillis()
 
-  val listSpells = get_n_spells(1,275)
+  val listSpells = get_n_spells(500,700)
 
-  if(listSpells(0).component == "V"){
-    println("correc'")
-  }
-  val tuple4_list = get_tuple4array_from_spells()       // convert full strings for levels to Tuple2(class:String,num:Integer)
-  var filtered_array = filter_spellscomponent()
-  var reduced_array = filter_spells(1,"(W|w)izard","V") // remove spells under level "max_lvl" and not from class "class_str"
-  var collected = reduced_array.collect()
+  var tuples = listSpells.map(current => {
+    var tuple = new Tuple4[Integer,String,String,String](current.spell_ID,current.name,current.level,current.component)
+    tuple
+  })
+
+  var component_filtered_tuples = tuples.filter(current_spell => {
+    if(current_spell._4 == "V"){
+      var levels = current_spell._2.split(" |, ")
+      true
+    }else{false}
+  })
 
   var test = 50
 
-  def filter_spellscomponent()
-
-  def filter_spells(max_lvl:Integer,class_str:String,component:String)={
-    sc.makeRDD(tuple4_list).filter(current_spell => {
-      val pattern = new Regex(class_str)
-      var lvl_ok = false
-      var class_ok = false
-      var component_ok = false
-      for(i <- current_spell.indices){
-        if(current_spell(i)._2 <= max_lvl){lvl_ok = true}
-        if( pattern.findAllMatchIn(current_spell(i)._1).toString == "non-empty iterator" ){class_ok=true}
-        if( current_spell(i)._4 == "V"){
-          component_ok = true
-        }
-      }
-      var result = lvl_ok&&class_ok&&component_ok
-      result
-    })
-  }
-
-  def get_tuple4array_from_spells()={
-    listSpells.toArray.map(current_spell => {
-      levelSTR_toArray(current_spell.level.split(" |, "),current_spell.spell_ID,current_spell.component)
-    })
-  }
-
   @throws(classOf[Exception])
-  def levelSTR_toArray(arg:Array[String],spell_ID:Integer,component:String)={
-    var list_levels = new ListBuffer[(String, Integer,Integer,String)]
+  def levelsArray_to_tuples(arg:Array[String])={
+    var list_levels = new ListBuffer[(Integer,String,String,String)]
     var current_string:String = new String("")
     var current_num:Integer = -1
-    var curseur = 0
-    println("arg.size : "+arg.length)
-    while(curseur < arg.length){
-      if(new Regex("[0-9]/[aA-zZ]").findAllMatchIn(arg(curseur)).toString() == "non-empty iterator"){
-        if(current_num== -1&&current_string!=""){
-          current_num = Integer.parseInt(arg(curseur).split("/")(0))
-          curseur-=1
-        }else if(current_num== -1&&current_string==""){
-          current_string = arg(curseur).split("/")(1)
-        }
-      }else {
-        try {
-          current_num = Integer.parseInt(arg(curseur))
-        } catch {
-          case exception: Exception =>
-            if (current_string == "") {
-              current_string = new String(arg(curseur))
-            } else {
-              throw new Exception("error in input n°" + curseur + "/spell_id : " + spell_ID + " : two string level in a row")
-            }
-        }
+    for(i <- arg.indices){
+      try {
+        current_num = Integer.parseInt(arg(i))
+      } catch {
+        case exception: Exception =>
+          if (current_string == "") {
+            current_string = new String(arg(i))
+          } else {
+            throw new Exception("error in input n°" + i + " : two string level in a row")
+          }
       }
 
       if(current_string != "" && current_num != -1){
-        var current_spell:(String,Integer,Integer,String) = new Tuple4[String,Integer,Integer,String](current_string,current_num,spell_ID,component)
+        var current_spell:(String,Integer) = new Tuple2[String,Integer](current_string,current_num)
         list_levels += current_spell
         current_string = ""
         current_num = -1
       }
-      curseur+=1
     }
     list_levels
   }
 
   def get_n_spells(n:Integer,first_id:Integer)={
-    var listSpells = new ListBuffer[Spell]
+    var listSpells = new ListBuffer[(Integer,String,String,String)]
     val url_base = "http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID="
     for(i <- 0 until n ){
       var html = Source.fromURL(url_base+(first_id+i))
       var s = html.mkString
-      listSpells+=new Spell(s,(first_id+i))
-      println("i : "+i)
-      if( (i%100)==0 ){
-        println("time : "+(System.currentTimeMillis() - time_debut))
-      }
+      var spell = new Spell(s,(first_id+i))
+      var spell_ID= first_id+i
+      listSpells += new Tuple4[Integer, String, String, String](spell_ID, spell.name, spell.level, spell.component )
+      println("i : " + spell_ID) // Temps réel de la récupération des sorts
     }
     listSpells
   }
